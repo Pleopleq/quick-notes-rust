bb5a3ea83bd03ea421ea02447e6fd25a8fd40c5a
Felix Anducho <felix.anducho74@gmail.com>
2023-02-19 20:19:52 -0600

    Get current working directory path

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+// use std::fs::{DirBuilder};
+use std::env::current_dir;
+use std::path::PathBuf;
+
+fn get_current_dir() -> PathBuf {
+    let path = current_dir();
+    return path.unwrap();
+}
+
+fn main()   {
+    let path = get_current_dir();
+    let notes_path = path.display();
+    println!("{}", notes_path)
 }
